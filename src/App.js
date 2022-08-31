bb1eaf1e2b3bb15a1cac75bfa0548b709de31875
BMdirac <busarimoruf20@gmail.com>
2022-08-31 20:52:40 +0100

    new changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Header } from './components';
+import { Header, Home } from './components';
 import { Destination, Crew, Technology } from './pages';
 
 const App = () => {
   return (
     <>
      <BrowserRouter>
+     <Header />
       <Routes>
-        <Header />
-        <Route path='/' />
+        <Route path='/' element={<Home /> }/>
         <Route path='/destination' element={<Destination /> }/>
         <Route path='/crew' element={<Crew /> }/>
         <Route path='/technology' element={<Technology /> }/>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,52 @@
 import React from 'react';
 import logo from'../starter-code/assets/shared/logo.svg';
 import { Link } from 'react-router-dom';
+import { CgMenuLeft } from 'react-icons/cg';
 
 const Header = () => {
+  const handleClick = () => {
+    const navbar = document.querySelector(".navbar");
+    const listItems = document.querySelectorAll(".list-item")
+
+    navbar.classList.toggle("open")
+    listItems.forEach((item) => item.addEventListener("click", () => navbar.classList.remove("open")))
+  }
   return (
     <>
-    <header>
+    <header className='header flex items-center justify-between w-full lg:flex-row'>
       <div>
-        <Link to='/'><img src={logo} alt='Space Tourism' title='Space Tourism'/></Link>
+        <Link to='/'><img src={logo} alt='Space Tourism' title='Space Tourism' /></Link>
+      </div>
+
+      <nav className='navbar'>
+        <ul>
+          <li className='list-item'>
+            <Link to='/' className='lg:text-white lg:flex lg:items-center'>
+              <span className='lg:text-4xl lg:font-bold mr-2'>00</span> Home
+            </Link>
+          </li>
+          <li className='list-item'>
+            <Link to='/destination' className='lg:text-white lg:flex lg:items-center'>
+              <span className='lg:text-4xl lg:font-bold mr-2'>01</span> Destination
+            </Link>
+          </li>
+          <li className='list-item'>
+            <Link to='/crew' className='lg:text-white lg:flex lg:items-center'>
+              <span className='lg:text-4xl lg:font-bold mr-2'>02</span> Crew
+            </Link>
+          </li>
+          <li className='list-item'>
+            <Link to='/technology' className='lg:text-white lg:flex lg:items-center'>
+              <span className='lg:text-4xl lg:font-bold mr-2'>03</span> Technology
+            </Link>
+          </li>
+        </ul>
+      </nav>
+       
+      <div className='lg:hidden'>
+        <button onClick={handleClick}>
+        <CgMenuLeft className="text-white text-4xl" />
+        </button>
       </div>
     </header>
     </>
